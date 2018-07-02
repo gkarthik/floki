3c45de89485c0f3ef3b282d75a6a99e91474f512
Garyk Brixi <garykbrixi@gmail.com>
2018-07-02 23:43:53 +0100

    allow comparing different samples

--- dashboard/app/scripts/controllers/about.js
+++ dashboard/app/scripts/controllers/about.js
@@ -10,9 +10,10 @@
 angular.module('dashboardApp')
   .controller('AboutCtrl', function ($scope, $window) {
     $scope.jsonOutputs = [
-      "json_output/PN1-C1-NS-A2-L1_S1_L001_R1_001.trim.dedup.json",
-      "json_output/PN2-C1-NS-A4-L1_S2_L001_R1_001.trim.dedup.json",
-      "json_output/PN4-C1-NS-A1-L1_S3_L001_R1_001.trim.dedup.json"
+      // "json_output/PN1-C1-NS-A2-L1_S1_L001_R1_001.trim.dedup.json",
+      // "json_output/PN2-C1-NS-A4-L1_S2_L001_R1_001.trim.dedup.json",
+      // "json_output/PN4-C1-NS-A1-L1_S3_L001_R1_001.trim.dedup.json"
+      "json_output/data.json"
     ];
     $scope.jsonData = {};
     var d3 = $window.d3;
--- dashboard/app/scripts/directives/taxonomytree.js
+++ dashboard/app/scripts/directives/taxonomytree.js
@@ -25,21 +25,23 @@ angular.module('dashboardApp')
 	var jsonFile = scope.jsonFile;
 	var padding = 20;
 	var d3 = $window.d3;
-	var width = $window.innerWidth, height = $window.innerHeight+500;
+  var margin = {top: 10, right: 300, bottom: 300, left: 30};
+	var width = $window.innerWidth - margin.left - margin.right, height = $window.innerHeight + 500;
 	var rawSvg = element.find("svg")[0];
 	var svg = d3.select(rawSvg);
 	d3.select(element.find("h3")[0]).html(jsonFile.split("/")[1].replace(".json", ""));
 	var base = 100;
-	svg.attr("width", width);
-	svg.attr("height", height);
-	var g = svg.append("svg:g");
+	svg.attr("width", width + margin.left+ margin.right);
+	svg.attr("height", height + margin.top + margin.bottom);
+	var g = svg.append("svg:g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 	var tree = d3.tree()
   .size([height-150, width-200]);
 	var colorScale = d3.scaleSequential(d3.interpolateRdYlBu);
 
   scope.runTree = function(){
 d3.json(scope.jsonFile, function(error, data){
-
 	scope.updateFilters = function(){
       root.children.forEach(collapseLevel);
       update(root);
@@ -77,46 +79,132 @@ d3.json(scope.jsonFile, function(error, data){
       .on("click", click)
     .attr("r", 5)
 	    .style("fill", colorFill);
+      function heatmap(d) {
+          var dataset = [];
+          var colors = ["red","orange","green","cyan","blue","purple","pink"];
+          for (var q=0; q<d.data.percentage.length; q++){
+            dataset[q]={}
+          dataset[q]["name"] = d.data.name[q];
+          dataset[q]["percentage"] = d.data.percentage[q];
+          dataset[q]["reads"] = d.data.reads[q];
+          dataset[q]["color"] = colors[q];
+        }
+        var map = g.append("svg:g")
+          .attr("class","heatmap")
+    	    .attr("transform", "translate("+parseInt(d.y)+","+parseInt(d.x + 20)+")");
+         map.append("rect")
+         .attr("width", 350)
+         .attr("height", 50)
+         .attr("stroke", "#000")
+         .attr("fill", "#FFF");
+        var bar = map.selectAll("bar")
+          .data(dataset)
+          .enter().append("svg:g")
+          .attr("transform", function(d, i){
+            return  "translate(" + i * barWidth + ",0)";
+        });
+        }
 	  nodeEnter.append("text")
 	    .attr("dy", 3)
 	    .attr("x", function(d) { return d.children ? -8 : 8; })
 	    .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
 	    .text(function(d) {
-          if(d.data.rank=="superkingdom"){
-      return d.data.name;
+          if(d.data.rank[0]=="superkingdom"){
+      return d.data.name[0];
           }
-          return d.children ? "" : d.data.name;
+          return d.children ? "" : d.data.name[0];
 	    });
-
 	  nodeEnter.on("mouseover", function(d, i){
-	    var t = g.append("svg:g")
-		.attr("class","tool-tip")
-	    	.attr("transform", "translate("+parseInt(d.y)+","+parseInt(d.x+10)+")");
-	    t.append("rect")
-	      .attr("height", 170)
-	      .attr("width", 350)
-	      .attr("stroke", "#000")
-	      .attr("fill", "#FFF");
-	    t.append("text")
-	      .attr("transform", "translate(2,12)")
-	      .selectAll("tspan")
-	      .data(function(){
-		var a = [d.data.name];
-		a.push("Tax ID: "+d.data.taxid);
-		a.push("Pass FDR Test: "+d.data.pass_fdr_test);
-		a.push("Corrected P-value: "+d.data.pvalue.toExponential());
-		a.push("Uncorrected P-value: "+d.data.uncorrected_pvalue.toExponential());
-		a.push("Reads: "+d.data.reads);
-		a.push("Control Reads: "+d.data.ctrl_reads);
-		a.push("Sample %: "+Math.round(d.data.percentage*1000000)/10000);
-		a.push("Ctrl %: "+Math.round(d.data.ctrl_percentage*1000000)/10000);
-		a.push(d.data.rank);
-		return a;
-	      })
-	      .enter().append("tspan")
-	      .attr("x", 4)
-	      .attr("y", function(d, i) { return 13 + i * 15; })
-	      .text(function(d) { return d; });
+      var dataset = [];
+      var colors = ["red","orange","green","cyan","blue","purple","pink"];
+      for (var q=0; q<d.data.percentage.length; q++){
+        dataset[q]={}
+      dataset[q]["name"] = d.data.file[q];
+      dataset[q]["percentage"] = d.data.percentage[q];
+      dataset[q]["reads"] = d.data.reads[q];
+      dataset[q]["color"] = colors[q];
+    }
+      var barWidth = 50;
+      var x = d3.scaleBand()
+          .range([0, 250])
+          .padding(0.1);
+      var y = d3.scaleLinear()
+     				.domain([0, d3.max(d3.values(dataset), function(d){return d.percentage;})])
+     				.range([0, 140]);
+      var t = g.append("svg:g")
+        .attr("class","tool-tip")
+  	    .attr("transform", "translate("+parseInt(d.y)+","+parseInt(d.x + 20)+")");
+       t.append("rect")
+       .attr("width", 350)
+       .attr("height", 250)
+       .attr("stroke", "#000")
+       .attr("fill", "#FFF");
+      var bar = t.selectAll("bar")
+        .data(dataset)
+        .enter().append("svg:g")
+        .attr("transform", function(d, i){
+          return  "translate(" + i * barWidth + ",0)";
+      });
+      var textbar = t.selectAll("textbar")
+        .data(dataset)
+        .enter().append("svg:g")
+        .attr("transform", function(d, i){
+          return  "translate(" + (i * barWidth) + "," + (142 - y(d.percentage)) +")";});
+      var format = d3.format(".2e");
+      textbar.append("text")
+        .attr("class", "textbar")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate(100," + 35 + ")")
+        .text(function(d) {
+          if (d.reads > 10000){
+            return format(d.reads);
+          }else {
+            return d.reads;
+        }})
+        .attr("font-family","sans-serif").style("font-size", "12px");
+
+      x.domain(dataset.map(function(d) { return d.name; }));
+
+      bar.append("rect")
+      .attr("y", function (a) {
+        return 151 - y(a.percentage);
+      })
+      .attr("transform", "translate(77," + 30 + ")")
+      .attr("width", barWidth - 10)
+      .attr("height", function(a){
+        return y(a.percentage);
+      })
+      .attr("fill", function(a){
+        return a.color;});
+
+    y.domain([d3.max(d3.values(dataset), function(d, i) {return (d.percentage * 100);}), 0])
+
+    t.append("g")
+    .attr("transform", "translate(70,185)")
+    .call(d3.axisBottom(x));
+
+    t.append("g")
+    .attr("transform", "translate(70,40)")
+    .call(d3.axisLeft(y));
+
+    t.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "translate(20,125)rotate(-90)")
+    .text("Percent of Reads")
+    .attr("font-family","sans-serif");
+
+    t.append("text")
+    .attr("transform",
+          "translate(" + 350/2 + " ,13 )")
+    .style("text-anchor", "middle")
+    .text("Percent and Number of Reads")
+    .attr("font-family","sans-serif");
+
+    t.append("text")
+    .attr("transform",
+          "translate(" + 350/2 + " ,230)")
+    .style("text-anchor", "middle")
+    .text("Samples").attr("font-family","sans-serif");
 	  });
 	  nodeEnter.on("mouseout", function(d, i){
 	    g.select(".tool-tip").remove();
@@ -126,18 +214,18 @@ d3.json(scope.jsonFile, function(error, data){
 	      .duration(duration)
 	      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
 	      .style("opacity", 1);
-
 	  nodeUpdate.select("circle").attr("r", function(d){
-	      return Math.log(d.data.reads);
+      return 5
+	      // return Math.log((d.data.reads[0]+d.data.reads[1]+d.data.reads[2]+d.data.reads[3]+d.data.reads[4])/5);
 	    })
 	    .style("fill", colorFill);
 	  nodeUpdate.select("text").style("text-anchor", function(d) { return d.children ? "end" : "start"; }).attr("dy", 3)
 	    .attr("x", function(d) { return d.children ? -8 : 8; })
 	    .text(function(d) {
-        if(d.data.rank=="superkingdom"){
-   return d.data.name;
+        if(d.data.rank[0]=="superkingdom"){
+   return d.data.name[0];
        }
-       return d.children ? "" : d.data.name;
+       return d.children ? "" : d.data.name[0];
      });
 
 	  node.exit().transition()
@@ -151,16 +239,17 @@ d3.json(scope.jsonFile, function(error, data){
 	    d.y0 = d.y;
 	  });
 	}
-	function colorFill(d){
-	      return colorScale(d.data.percentage);
+  	function colorFill(d){
+	      // return colorScale(d.data.percentage[0]);
+        return colorScale((d.data.percentage[0]+d.data.percentage[1]+d.data.percentage[2]+d.data.percentage[3]+d.data.percentage[4]));
 	}
 	function runThreholdFilters(node){
 	  if(scope.fdrFilter){
-	    if(!node.pass_fdr_test || node.reads <= scope.readsThreshold || node.percentage <= node.ctrl_percentage * scope.ratioThreshold){
+	    if(!node.pass_fdr_test[0] || !node.pass_fdr_test[1] || !node.pass_fdr_test[2] || !node.pass_fdr_test[3] || !node.pass_fdr_test[4] || node.reads[0] <= scope.readsThreshold || node.percentage[0] <= node.ctrl_percentage[0] * scope.ratioThreshold){
 	      return 0;
 	    }
 	  } else {
-	    if(node.uncorrected_pvalue > scope.pvalueThreshold || node.reads <= scope.readsThreshold || node.percentage <= node.ctrl_percentage * scope.ratioThreshold){
+	    if(node.uncorrected_pvalue[0] > scope.pvalueThreshold || node.reads[0] <= scope.readsThreshold || node.percentage[0] <= node.ctrl_percentage[0] * scope.ratioThreshold){
 	      return 0;
 	    }
 	  }
@@ -187,7 +276,6 @@ d3.json(scope.jsonFile, function(error, data){
 	}
 
   function click(d){
-    console.log(scope.ratioThreshold);
     if (d.children) {
       d._children = d.children;
       d.children = null;
@@ -202,19 +290,12 @@ d3.json(scope.jsonFile, function(error, data){
         d.children = d._children;
         d._children = null;
       }
-      if (d.children && d.data.rank == scope.taxFilter) {
+      if (d.children && d.data.rank[0] == scope.taxFilter) {
           d._children = d.children;
           d._children.forEach(collapseLevel);
           d.children = null;
       } else if (d.children) {
           d.children.forEach(collapseLevel);
-      }
-  }
-  function collapseAll(d) {
-      if (d.children) {
-          d.children.each(click);
-      } else if (d._children) {
-          d.children.forEach(collapseAll);
       }
   }
   getSignificantNodes(data);
