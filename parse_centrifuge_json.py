69cebf242ead6ef766a559131e9f778e0559227c
gkarthik <xraydiffraction@gmail.com>
2018-08-29 20:24:45 -0700

    Added centrifuge output parse script

---
+++ parse_centrifuge_json.py
@@ -0,0 +1,47 @@
+import imp
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import taxon_tree
+import json
+
+centrifuge_output = pd.read_table("~/hpc_downloads/2018.08.06/centrifuge_report.tsv")
+centrifuge_output2 = pd.read_table("~/hpc_downloads/2018.08.06/centrifuge_report_2.tsv")
+annotations = pd.read_csv("./merged_disease_pathogen_symptom_annotations.csv")
+annotations = annotations.fillna("")
+
+nodes_cols = ["tax_id", "parent_tax_id", "rank", "embl_code", "div_id", "inherited_div_flag", "genetic_code_id", "inherited_gc_flag", "mito_genetic_code_id", "inherited_mito_flag", "genbank_flag_id", "hidden_subtree_flag", "comments", "none"]
+names_cols = ["tax_id", "name_txt", "unique_name", "name_class", "none"]
+
+names_src="taxdmp/names.dmp"
+nodes_src="taxdmp/nodes.dmp"
+names_df = pd.read_table(names_src, sep="|", names = names_cols)
+nodes_df = pd.read_table(nodes_src, sep="|", names = nodes_cols)
+
+names_df = names_df.apply(lambda x: x.str.strip() if x.dtype == np.object else x)
+names_df = names_df[names_df["name_class"] == "scientific name"]
+
+nodes_df = nodes_df.apply(lambda x: x.str.strip() if x.dtype == np.object else x)
+
+nodes_df = nodes_df.set_index("tax_id")
+names_df = names_df.set_index("tax_id")
+
+imp.reload(taxon_tree)
+root = taxon_tree.Node(1, None, "Root", "no rank")
+root.populate_taxonomy(centrifuge_output, nodes_df, names_df, root)
+root.populate_annotations(annotations)
+root.dump_object()
+
+# Batch samples
+root.populate_with_reads(centrifuge_output)
+root.populate_with_reads(centrifuge_output2)
+
+# Ctrl reads
+root.populate_ctr_reads(centrifuge_output2)
+
+# Compute reads at every taxon level
+root.populate_reads_at_taxon()
+
+root.compute_pvalues()
+d = root.to_dict()
---
+++ taxon_tree.py
@@ -0,0 +1,263 @@
+import pickle as pickle
+import numpy as np
+import scipy.stats as stats
+
+class Node:
+
+    def __init__(self, id, p, n, r):
+        # Taxnomy Annotations
+        self.tax_id = id
+        self.taxon_name = n
+        if p!= None:
+            self.parent = p
+            self.depth = self.parent.depth + 1
+            p.children.append(self)
+        else:
+            self.parent = None
+            self.depth = 0
+        self.rank =r
+        self.children = []
+
+        # Ctrl metrics
+        self.ctrl_reads = 0
+        self.ctrl_percentage = 0
+
+        # Sample metrics
+        self.reads = []
+        self.taxon_reads = []
+        self.percentage = []
+        self.unique_reads = []
+        self.file = []
+        self.pvalue = []
+        self.odssratio = []
+        self.uncorrected_pvalue = []
+
+        # Ontology Annotations
+        self.disease = []
+        self.disease_label = []
+        self.pathogenic = False
+        self.symptom = []
+        self.symptom_label = []
+
+        # Other Annotations
+        self.genome_size = 0
+
+    def set_parent(self, p):
+        self.parent = p
+        self.depth = self.parent.depth + 1
+        p.children.append(self)
+
+    def add_child(self, n):
+        self.children.append(n)
+
+    def print_tree(self):
+        if(any([i > 0 for i in self.reads if i != None])):
+            print(" "*self.depth + self.taxon_name + " " + ",".join([str(i) for i in self.reads])  + " " + ",".join([str(round(i*100, 2)) +"%" for i in self.percentage]))
+        for i in self.children:
+            i.print_tree()
+
+    def get_node(self, id):
+        if self.tax_id == id:      # Check if id matches current node
+            return self
+        for i in self.children:    # Check if id matches any of the children
+            _ = i.get_node(id)
+            if _ != None:
+                return _
+        return None
+
+    def populate_taxonomy(self, reads_df, nodes_df, names_df, root):
+        for _i, i in enumerate(reads_df["taxID"].tolist()): # get tax IDS from centrifuge output
+            if _i % 100000 == 0:
+                print(_i)
+            n = root.get_node(i)
+            tax_id = i
+            while(n == None):
+                if tax_id not in nodes_df.index.values:
+                    print(str(tax_id)+" not in NCBI taxonomy. Please check to see if the tax id was merged into another and replace the old id")
+                    break
+                n = Node(tax_id, None, names_df.ix[tax_id]["name_txt"], nodes_df.ix[tax_id]["rank"])
+                p = root.get_node(nodes_df.ix[tax_id]["parent_tax_id"])
+                if p != None:
+                    n.set_parent(p)
+                tax_id = nodes_df.ix[tax_id]["parent_tax_id"]
+                n = p
+
+    def get_total_children(self):
+        ctr = 1
+        for i in self.children:
+            ctr += i.get_total_children()
+        return ctr
+
+    def get_children_with_reads(self):
+        ctr = 0
+        if(any([i > 0 for i in self.reads if i != None])):
+            ctr += 1
+        for i in self.children:
+            ctr += i.get_children_with_reads()
+        return ctr
+
+    def dump_object(self):
+        with open('taxobject.pkl', 'wb') as output:
+            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
+        output.close()
+
+    def read_object(self):
+        _ = None
+        with open('taxobject.pkl', 'rb') as input:
+            _ = pickle.load(input)
+        input.close()
+        return _
+
+    def init_new_sample(self, name):
+        if name == None:
+            name = "Sample " + str(len(self.file) + 1)
+        self.reads.append(None)
+        self.percentage.append(None)
+        self.unique_reads.append(None)
+        self.file.append(name)
+        self.pvalue.append(None)
+        self.uncorrected_pvalue.append(None)
+        for i in self.children:
+            i.init_new_sample(name)
+
+    def get_total_reads(self):
+        _ = [0] * len(self.reads)
+        for i in range(len(self.reads)):
+            if self.reads[i] != None:
+                _[i] += self.reads[i]
+        for i in self.children:
+            for _k, k in enumerate(i.get_total_reads()):
+                _[_k] += k
+        return _
+
+    def populate_percentage(self, total_count):
+        for _i, i in enumerate(self.reads):
+            if i != None:
+                self.percentage[_i] = i/total_count[_i]
+        for i in self.children:
+            i.populate_percentage(total_count)
+
+    def populate_with_reads(self, df, name = None):
+        self.init_new_sample(name)
+        for i in df.index.values:
+            _ = df.ix[i]
+            n = self.get_node(_["taxID"])
+            if n == None:
+                continue
+            if n.reads[-1] == None:
+                n.reads[-1] = 0
+            n.reads[-1] += _["numReads"]
+            if n.unique_reads[-1] == None:
+                n.unique_reads[-1] = 0
+            n.unique_reads[-1] += _["numUniqueReads"]
+        if self.tax_id == 1:
+            self.populate_percentage(self.get_total_reads())
+
+    def print_read_metrics(self):
+        for i in range(len(self.reads)):
+            print("File: " + self.file[i])
+            print("Reads: " + str(self.reads[i]))
+            print("Percentage: " + str(self.percentage[i]))
+            print("Unique reads: " + str(self.unique_reads[i]))
+            print("Pvalue:" + str(self.pvalue[i]))
+            print("Uncorrected pvalue: "+ str(self.uncorrected_pvalue[i]))
+
+    def print_annotations(self):
+        if self.pathogenic:
+            print(" "*self.depth + self.taxon_name)
+            print(" "*self.depth + "Diseases: " + ",".join(self.disease_label))
+            print(" "*self.depth + "Symptoms: " ",".join(self.symptom_label))
+        for i in self.children:
+            i.print_annotations()
+
+    def populate_ctr_reads(self, df):
+        for i in df.index.values:
+            _ = df.ix[i]
+            n = self.get_node(_["taxID"])
+            if n == None:
+                continue
+            if n.ctrl_reads == None:
+                n.ctrl_reads = 0
+            n.ctrl_reads += _["numReads"]
+        if self.tax_id == 1:
+            total = self.get_total_ctrl_reads()
+            self.populate_ctrl_percentage(total)
+
+    def populate_ctrl_percentage(self, total):
+        self.ctrl_percentage = self.ctrl_reads/total
+        for i in self.children:
+            i.populate_ctrl_percentage(total)
+
+    def get_total_ctrl_reads(self):
+        ctr = self.ctrl_reads
+        for i in self.children:
+            ctr += i.get_total_ctrl_reads()
+        return ctr
+
+    def populate_reads_at_taxon(self):
+        self.taxon_reads = self.get_total_reads()
+        for i in self.children:
+            i.populate_reads_at_taxon()
+
+    def populate_annotations(self, df):
+        _ = df[df["tax_id"] == self.tax_id]
+        if _.shape[0] > 0:
+            self.pathogenic = True
+        for i in _.index.values:
+            self.disease.append(_.ix[i]["disease"])
+            _disease_label = _.ix[i]["disease_label_doid"]
+            if _disease_label == "":
+                _disease_label = _.ix[i]["disease_label_mondo"]
+            self.disease_label.append(_disease_label)
+            self.symptom.append(_.ix[i]["symptom"])
+            self.symptom_label.append(_.ix[i]["symptom_label"])
+        for i in self.children:
+            i.populate_annotations(df)
+
+    # Fisher's eaxt
+    #         | Sample | Ctrl
+    # Species |
+    # Total   |
+    def compute_pvalues(self):
+        pval = [1] * len(self.taxon_reads)
+        oddsratio = [1] * len(self.taxon_reads)
+        _t = self.get_total_reads()
+        for _i, i in enumerate(self.taxon_reads):
+            _ = stats.fisher_exact([[i, self.ctrl_reads], [_t[_i], self.get_total_ctrl_reads()]])
+            oddsratio[_i] = _[0]
+            pval[_i] = _[1]
+        self.pvalue = pval
+        self.odssratio = oddsratio
+        for i in self.children:
+            i.compute_pvalues()
+
+    def to_dict(self):
+        d = {
+            # Taxonomy
+        "tax_id": self.tax_id,
+            "taxon_name": self.taxon_name,
+            "parent": self.parent.to_dict() if self.parent != None else None,
+            "depth": self.depth,
+            "rank": self.rank,
+            # Ctrl
+        "ctrl_reads": self.ctrl_reads,
+            "ctrl_percentage": self.ctrl_percentage,
+            # Sample
+        "reads": self.reads,
+            "taxon_reads": self.taxon_reads,
+            "percentage": self.percentage,
+            "unique_reads": self.unique_reads,
+            "file": self.file,
+            "pvalue": self.pvalue,
+            "oddsratio": self.oddsratio,
+            "uncorrected_pvalue": self.pvalue, # For now no multiple hypothesis testing. To add.
+            # Ontology Annotation
+        "disease": self.disease,
+            "disease_label": self.disease_label,
+            "pathogenic": self.pathogenic,
+            "symptom": self.symptom,
+            "symptom_label": self.symptom_label,
+            "genomoe_size": self.genome_size,
+            "children": [i.to_dict() for i in self.children]
+        }
+        return d
