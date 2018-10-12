ae1b51285a77181d9bad7b72b89b377db906b7a2
gkarthik <xraydiffraction@gmail.com>
2018-10-11 17:36:12 -0700

    Changed pathogenic annotation to allow vertical scaling.

--- dashboard/app/scripts/directives/annotation.js
+++ dashboard/app/scripts/directives/annotation.js
@@ -22,14 +22,14 @@ angular.module('dashboardApp')
 	    height = window.innerHeight,
 	    context,
 	    canvas_wrapper =  d3.select("#annotation-wrapper");
-
+	
 	var annotated_heatmap = {
 	  "padding": 5,
 	  "square_size": 20,
-	  "width": window.innerWidth,
-	  "height": window.innerHeight,
-	  "offset_x": window.innerWidth/6,
-	  "offset_y": window.innerHeight/10
+	  "width": window.innerWidth/2,
+	  "height": window.innerHeight * 2,
+	  "offset_x": window.innerWidth/12,
+	  "offset_y": window.innerHeight/6
 	};
 
 	function setup_canvas(id, width, height) {
@@ -60,24 +60,21 @@ angular.module('dashboardApp')
 	function draw_heatmap_annotated(d){
 
 	  var annotated_nodes = get_all_annotated_nodes(d, "pathogenic");
-	  // annotated_heatmap.width = annotated_nodes.length * (annotated_heatmap.square_size + annotated_heatmap.padding * 2);
-    annotated_heatmap.height = annotated_nodes.length % width_fit_number * (100);
-	  width = annotated_heatmap.width;
-    height = annotated_heatmap.height + annotated_heatmap["offset_y"] * 2;
+	  
+	  annotated_heatmap.width = annotated_nodes.length * (annotated_heatmap.square_size + annotated_heatmap.padding * 2);
+	  
+	  annotated_heatmap.cell_height = 200 + annotated_nodes[0].percentage.length * annotated_heatmap.square_size;
+	  
+	  var x = d3.scaleBand()
+	      .rangeRound([0, annotated_heatmap.width])
+	      .domain(annotated_nodes.map(function(x){return x.taxon_name;}));
+	  
+	  height = annotated_heatmap.offset_y + x(annotated_nodes[annotated_nodes.length - 1].taxon_name)/(width - 2 * annotated_heatmap.offset_x) * annotated_heatmap.cell_height + annotated_heatmap.offset_x;
+	  
 	  context = setup_canvas("annotation-wrapper", width, height);
 	  var node = canvas_wrapper.selectAll(".annotated-node").data(annotated_nodes, function(d){
 	    return d;
 	  });
-
-    var width_fit_number = (annotated_heatmap.width - annotated_heatmap["offset_x"])/(annotated_heatmap.square_size + annotated_heatmap.padding * 2)
-
-	  var x = d3.scaleBand()
-	      .rangeRound([15, annotated_heatmap.width])
-        .domain(d3.range(width_fit_number));
-
-    var y = d3.scaleBand()
-        .rangeRound([annotated_heatmap.offset_y, annotated_nodes.length%width_fit_number])
-        .domain(d3.range(Math.ceil(annotated_nodes.length/width_fit_number)));
 
 	  var _min = Math.min.apply(Math, annotated_nodes.map(function(x){
 	    return Math.min.apply(Math, x.percentage);
@@ -94,13 +91,10 @@ angular.module('dashboardApp')
 	      .append("annotated-node")
 	      .classed("annotated-node", true)
 	      .attr("x", function(d){
-          console.log(x(Math.floor(annotated_nodes.indexOf(d) % width_fit_number)))
-		return x(Math.floor(annotated_nodes.indexOf(d) % width_fit_number));
+		return x(d.taxon_name) % (width - 2 * annotated_heatmap.offset_x) + annotated_heatmap.offset_x;
 	      })
 	      .attr("y", function(d){
-          console.log(y(Math.floor(annotated_nodes.indexOf(d)/width_fit_number)))
-          // console.log(y(Math.floor(annotated_nodes.indexOf(d)/width_fit_number)))
-		return y(Math.floor(annotated_nodes.indexOf(d)/width_fit_number));
+		return Math.floor(x(d.taxon_name) / (width - 2 * annotated_heatmap.offset_x)) * annotated_heatmap.cell_height + annotated_heatmap.offset_y;
 	      })
 	      .text(function(d){
 		return d.taxon_name;
@@ -117,24 +111,23 @@ angular.module('dashboardApp')
 	    context.strokeStyle = "#000000";
 	    context.lineWidth = 2;
 	    for (var i = 0; i < d.percentage.length; i++) {
-	      context.rect(x(_node.text()),  annotated_heatmap.offset_y +(i*annotated_heatmap.square_size), annotated_heatmap.square_size, annotated_heatmap.square_size);
+	      context.rect(parseFloat(_node.attr("x")), parseFloat(_node.attr("y")) + (i * annotated_heatmap.square_size), annotated_heatmap.square_size, annotated_heatmap.square_size);
 	      context.fillStyle = percentage_scale(d.percentage[i]);
 	      context.fill();
 	      context.stroke();
 	    }
-      context.save();
-      context.translate(5 + x(_node.text()), 5 + annotated_heatmap.offset_y + (i*annotated_heatmap.square_size));
-      context.rotate(-Math.PI/2);
-      context.translate(-1*(x(_node.text())), -1 * (annotated_heatmap.offset_y + (i*annotated_heatmap.square_size)));
+	    context.save();
+	    context.translate(parseFloat(_node.attr("x")), parseFloat(_node.attr("y")));
 	    context.fillStyle="#000000";
 	    context.font = "12px Helvetica";
-	    context.textAlign = "right";
-	    context.textBaseline = "middle";
-	    context.fillText(_node.text(), x(_node.text()), annotated_heatmap.offset_y + (i*annotated_heatmap.square_size));
-      context.restore();
+	    context.textAlign = "left";
+	    context.textBaseline = "top";
+	    context.rotate(-1 * Math.PI/3);
+	    context.fillText(_node.text(), 0, 0);
+	    context.restore();
 	    context.closePath();
 	  });
-
+	  
 	}
 
 	d3.json(scope.jsonFile, function(error, data){
