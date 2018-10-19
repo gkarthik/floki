3df980381bdda1da64f2e5682c9551c1fa5377ef
gkarthik <xraydiffraction@gmail.com>
2018-10-18 20:47:45 -0700

    Added barchart on hover for pathogenic annotations.

--- dashboard/app/scripts/directives/annotation.js
+++ dashboard/app/scripts/directives/annotation.js
@@ -16,15 +16,12 @@ angular.module('dashboardApp')
       },
       link: function postLink(scope, element, attrs) {
 	var d3 = $window.d3,
-	    jQuery = $window.jQuery,
-	    json = jQuery.extend(scope.jsonData),
 	    width = window.innerWidth,
 	    height = window.innerHeight,
 	    context,
-	    y_pos,
-	    x_pos,
 	    canvas_wrapper =  d3.select("#annotation-wrapper"),
-	    annotated_nodes;
+	    annotated_nodes,
+	    scale_x;
 
 	var annotated_heatmap = {
 	  "padding": 5,
@@ -70,21 +67,6 @@ angular.module('dashboardApp')
 
 
 	function draw_heatmap_annotated(annotated_nodes){
-
-	  annotated_heatmap.width = annotated_nodes.length * (annotated_heatmap.square_size + annotated_heatmap.padding * 2);
-
-	  annotated_heatmap.cell_height = 200 + annotated_nodes[0].percentage.length * annotated_heatmap.square_size;
-
-	  var x = d3.scaleBand()
-	      .rangeRound([0, annotated_heatmap.width])
-	      .domain(annotated_nodes.map(function(x){return x.taxon_name;}));
-
-	  height = annotated_heatmap.offset_y + x(annotated_nodes[annotated_nodes.length - 1].taxon_name)/(width - 2 * annotated_heatmap.offset_x) * annotated_heatmap.cell_height + annotated_heatmap.offset_x;
-
-	  var node = canvas_wrapper.selectAll(".annotated-node").data(annotated_nodes, function(d){
-	    return d;
-	  });
-
 	  var _min = Math.min.apply(Math, annotated_nodes.map(function(x){
 	    return Math.min.apply(Math, x.percentage);
 	  }));
@@ -95,23 +77,6 @@ angular.module('dashboardApp')
 
 	  var percentage_scale = d3.scaleSequential(d3.interpolateYlOrRd)
 	      .domain([_min, _max]);
-
-	  var nodeEnter = node.enter()
-	      .append("annotated-node")
-	      .classed("annotated-node", true)
-	      .attr("x", function(d){
-		return x(d.taxon_name) % (width - 2 * annotated_heatmap.offset_x) + annotated_heatmap.offset_x;
-	      })
-	      .attr("y", function(d){
-		return Math.floor(x(d.taxon_name) / (width - 2 * annotated_heatmap.offset_x)) * annotated_heatmap.cell_height + annotated_heatmap.offset_y;
-	      })
-	      .text(function(d){
-		return d.taxon_name;
-	      });
-
-	  var nodeUpdate = nodeEnter.merge(node);
-
-	  var nodeExit = node.exit().remove();
 
 	  var _node;
 	  context.clearRect(0, 0, width, height);
@@ -152,8 +117,7 @@ angular.module('dashboardApp')
 	    .each(function(d){
 	      _node = d3.select(this);
 	      if(_node.attr("hover-status")=="active"){
-		console.log(d.taxon_name);
-		draw_hover(d, d3.select(this), ["percentage", "taxon_reads"]);
+		draw_hover(d, _node, ["percentage", "taxon_reads"]);
 	      }
 	    });
 	}
@@ -163,11 +127,10 @@ angular.module('dashboardApp')
 	  d3.selectAll(".annotated-node")
 	    .each(function(d){
 	      n = d3.select(this);
-	      n.attr("hover-status", "inactive");
 	      if(within_radius([parseFloat(n.attr("x"))+annotated_heatmap.square_size/2, parseFloat(n.attr("y"))], coords, annotated_heatmap.square_size/2)){
 		n.attr("hover-status", "active");
 	      } else {
-
+		n.attr("hover-status", "inactive");
 	      }
 	    });
 	  draw_heatmap_annotated(nodes);
@@ -194,11 +157,11 @@ angular.module('dashboardApp')
 
 	  var _width = barchart.width/keys.length;
 	  for (var i = 0; i < keys.length; i++) {
-	    draw_barchart(d, keys[i], (_width + barchart.padding) * i, 0, d3.schemeDark2[i]);
+	    draw_barchart(d, node, keys[i], (_width + barchart.padding) * i, 0, d3.schemeDark2[i]);
 	  }
 	}
 
-	function draw_barchart(d, key, offset_x, offset_y, _color){
+	function draw_barchart(d, node, key, offset_x, offset_y, _color){
 	  offset_y = (offset_y == null) ? 0 : offset_y;
 	  offset_x = (offset_x == null) ? 0 : offset_x;
 
@@ -208,8 +171,8 @@ angular.module('dashboardApp')
 	  var bar_y = d3.scaleLinear()
 	      .rangeRound([0, barchart.height]);
 
-	  var _x = d.y + 50 + offset_x;
-	  var _y = d.x + 20 + offset_y;
+	  var _x = parseFloat(node.attr("x")) + 50 + offset_x;
+	  var _y = parseFloat(node.attr("y")) + 20 + offset_y;
 	  bar_x.domain(d["file"]);
 	  var y_domain_elmns = d[key].slice();
 	  y_domain_elmns.push(d["ctrl_"+key]); // Get ctrl value as well
@@ -257,7 +220,11 @@ angular.module('dashboardApp')
 	    context.lineTo(_x + barchart.padding - 6, _y + barchart.padding + barchart.height - bar_y(d) + 0.5);
 	    context.textAlign = "right";
 	    context.textBaseline = "middle";
-	    context.fillText((d * 100).toExponential(1), _x + barchart.padding - 6, _y + barchart.padding + barchart.height - bar_y(d) + 0.5);
+	    if(key=="percentage"){
+	      context.fillText((d * 100).toExponential(1), _x + barchart.padding - 6, _y + barchart.padding + barchart.height - bar_y(d) + 0.5);
+	    } else {
+	      context.fillText(d.toExponential(1), _x + barchart.padding - 6, _y + barchart.padding + barchart.height - bar_y(d) + 0.5);
+	    }
 	  });
 	  context.strokeStyle = "#000000";
 	  context.stroke();
@@ -274,10 +241,46 @@ angular.module('dashboardApp')
 	  context.stroke();
 	}
 
+	function update(nodes){
+	  annotated_heatmap.width = annotated_nodes.length * (annotated_heatmap.square_size + annotated_heatmap.padding * 2);
+	  
+	  annotated_heatmap.cell_height = 200 + annotated_nodes[0].percentage.length * annotated_heatmap.square_size;
+
+	  scale_x = d3.scaleBand()
+	      .rangeRound([0, annotated_heatmap.width])
+	    .domain(annotated_nodes.map(function(x){return x.taxon_name;}));
+
+	  height = annotated_heatmap.offset_y + scale_x(annotated_nodes[annotated_nodes.length - 1].taxon_name)/(width - 2 * annotated_heatmap.offset_x) * annotated_heatmap.cell_height + annotated_heatmap.offset_x;
+
+	  var node = canvas_wrapper.selectAll(".annotated-node").data(annotated_nodes, function(d){
+	    return d;
+	  });
+
+	  
+	  var nodeEnter = node.enter()
+	      .append("annotated-node")
+	      .classed("annotated-node", true)
+	      .attr("x", function(d){
+		return scale_x(d.taxon_name) % (width - 2 * annotated_heatmap.offset_x) + annotated_heatmap.offset_x;
+	      })
+	      .attr("y", function(d){
+		return Math.floor(scale_x(d.taxon_name) / (width - 2 * annotated_heatmap.offset_x)) * annotated_heatmap.cell_height + annotated_heatmap.offset_y;
+	      })
+	      .text(function(d){
+		return d.taxon_name;
+	      });
+
+	  var nodeUpdate = nodeEnter.merge(node);
+
+	  var nodeExit = node.exit().remove();
+	  
+	  draw_heatmap_annotated(nodes);
+	}
+
 	d3.json(scope.jsonFile, function(error, data){
 	  context = setup_canvas("annotation-wrapper", width, height);
 	  annotated_nodes = get_all_annotated_nodes(data, "pathogenic");
-	  draw_heatmap_annotated(annotated_nodes);
+	  update(annotated_nodes);
 	  d3.select("#annotation-wrapper")
 	    .on("mousemove", function(d){
               var coords = d3.mouse(this);
